4ec8ca39fdaeff1db1fdde762b16db1a44d153d4
Boris Silva <boryssilva0202@gmail.com>
2024-05-15 15:16:12 -0500

    End section tree

--- src/main/java/com/borissilvam/securitybasic/config/SecurityConfig.java
+++ src/main/java/com/borissilvam/securitybasic/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.borissilvam.securitybasic.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,35 +23,56 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(
+                (csrfConfigurer)->  csrfConfigurer.disable()
+        );
         http.authorizeHttpRequests(
                 (requests) -> requests
                                 .requestMatchers("myAccount", "myBalance", "myCards", "myLoans").authenticated()
-                                .requestMatchers("myContact", "myNotices").permitAll()
+                                .requestMatchers("myContact", "myNotices", "/register").permitAll()
         );
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
 
-        
+    }
 
-        //Negar el acceso a cualquier peticion
-//        http.authorizeHttpRequests(
-//                (requests) -> requests.anyRequest().denyAll()
-//        );
-//        http.formLogin(withDefaults());
-//        http.httpBasic(withDefaults());
-//        return http.build();
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsServices(){
 //
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("12345")
+////                .authorities("admin")
+////                .build();
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("read")
+////                .build();
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+//        //Enfoque 2 de como crear usando otro Bean para la configuración de las contraseñas
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withUsername("boris")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
-        //Permitir el acceso a cualquier peticion
-//        http.authorizeHttpRequests(
-//                (requests) -> requests.anyRequest().permitAll()
-//        );
-//        http.formLogin(withDefaults());
-//        http.httpBasic(withDefaults());
-//        return http.build();
-
-
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
---
+++ src/main/java/com/borissilvam/securitybasic/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.borissilvam.securitybasic.repository;
+
+import com.borissilvam.securitybasic.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+
+}
